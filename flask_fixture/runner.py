d042e45ce91cf467c7e6e94f2facebefce62e4a9
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-12 01:39:00 +0300

    bottle

--- flask_fixture/runner.py
+++ flask_fixture/runner.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from bottle import route, run, template
 
 from flask_fixture.collection_of_routes import routes
 
@@ -7,13 +7,11 @@ def run_flask(queue, port, app_fabric):
     """
     Функция запускает сервер Flask для выполнения тестов.
     """
-    app = app_fabric('flask_fixture')
-
     for route in routes:
         print('add route', route)
-        routing = app.route(*(route.args), **(route.kwargs))
+        routing = route(*(route.args), **(route.kwargs))
         routing(route.function)
 
     queue.put(None)
 
-    app.run(port=port)
+    run(host='localhost', port=port)
