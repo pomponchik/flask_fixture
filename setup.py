346aca7782d1c7c04e3ec91a1e0b9cebeea159ca
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-11 20:15:58 +0300

    new version of the flask

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf8') as readme_file:
     readme = readme_file.read()
 
-requirements = ['flask==2.2.5']
+requirements = ['flask==2.0.0']
 
 setup(
     name='flask_fixture',
