b9022b5a101dcb312dfe41d4535c9173d55bfb3e
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-12 01:00:51 +0300

    print

--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -11,4 +11,5 @@ def root():
 def test_run_server(local_server_url):
     from time import sleep
     sleep(2.0)
+    print(requests.get(local_server_url).text)
     assert requests.get(local_server_url).text == 'kek'
