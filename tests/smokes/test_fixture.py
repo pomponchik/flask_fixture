09ef3ce3682ac9ca027bd01b208ead1574aaa7fa
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 00:12:00 +0300

    test without some trash

--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -1,14 +1,5 @@
-import os
-
-import pytest
 import requests
 
 
-@pytest.mark.timeout(60)
 def test_run_server(local_server_url, root):
-    from time import sleep
-
-    print('url:', local_server_url)
-    print(requests.get(local_server_url).text)
-    print('THIS PID:', os.getpid())
     assert requests.get(local_server_url).text == 'kek'
