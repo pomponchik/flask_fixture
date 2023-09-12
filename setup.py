d2a640407b611a8b72dfcb1c9f71aabef890d449
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-12 20:22:45 +0300

    dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf8') as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = ['flask']
 
 setup(
     name='flask_fixture',
