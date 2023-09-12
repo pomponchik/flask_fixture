1b18fa8e75b21af27457adc19f7f28bca8434544
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 00:13:03 +0300

    test without some trash

--- tests/smokes/conftest.py
+++ tests/smokes/conftest.py
@@ -1,7 +1,7 @@
 from flask_fixture import endpoint
 import pytest
 
-@pytest.fixture
+
 @endpoint('/')
 def root():
     return 'kek'
--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -1,5 +1,5 @@
 import requests
 
 
-def test_run_server(local_server_url, root):
+def test_run_server(local_server_url):
     assert requests.get(local_server_url).text == 'kek'
