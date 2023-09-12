85c8af5b9cf97962effaf6f11d35a6a06711b5c9
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-12 14:41:39 +0300

    timeout

--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -1,11 +1,13 @@
-import requests
 import os
 
+import pytest
+import requests
 
 
+@pytest.mark.timeout(60)
 def test_run_server(local_server_url, root):
     from time import sleep
-    sleep(10.0)
+
     print('url:', local_server_url)
     print(requests.get(local_server_url).text)
     print('THIS PID:', os.getpid())
