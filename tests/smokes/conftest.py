af546958b3885f83b84f6b15bb0ab0b3a67d8dc8
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 16:20:09 +0300

    test with a simple template

--- tests/smokes/conftest.py
+++ tests/smokes/conftest.py
@@ -1,7 +1,24 @@
-from flask_fixture import endpoint
+import os
+
 import pytest
+from flask import render_template
+
+from flask_fixture import endpoint
 
 
 @endpoint('/')
 def root():
     return 'kek'
+
+
+@endpoint('/simple_template')
+def simple_template():
+    return render_template(
+        os.path.join(
+            'tests',
+            'smokes',
+            'data',
+            'templates',
+            'index.html',
+        )
+    )
--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -1,7 +1,18 @@
+import urllib
+
 import requests
 import pytest
+
+import urllib.parse
+>>> urllib.parse.urljoin(url1, url2)
 
 
 @pytest.mark.timeout(30)
 def test_run_server(local_server_url):
     assert requests.get(local_server_url).text == 'kek'
+
+
+@pytest.mark.timeout(30)
+def test_render_simple_template(local_server_url):
+    page_url = urllib.parse.urljoin(local_server_url, 'simple_template')
+    assert requests.get(page_url).text
