58a52e8ce68352eebc4679025cedc2b2ccef242a
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 04:47:55 +0300

    type hints, docstring

--- flask_fixture/runner.py
+++ flask_fixture/runner.py
@@ -1,12 +1,13 @@
 import os
 from contextlib import redirect_stdout, redirect_stderr
+from multiprocessing import Queue
 
 from flask import Flask
 
 from flask_fixture.collection_of_routes import routes
 
 
-def run_flask(queue, port, modules):
+def run_flask(queue: Queue, port: int, modules: list[str]):
     """
     The function is designed to run in a separate process. It starts the flask server.
 
@@ -15,6 +16,8 @@ def run_flask(queue, port, modules):
     2. The Flask application is created and all the endpoints collected in the first step are registered in it.
 
     The server output (both stderr and stdout) is redirected to the trash.
+
+    The queue is used to synchronize with the process that started this function. After the function is started, that process starts waiting for the function to put something in the queue. Thus, it is guaranteed that the starting process will not continue its execution before the server initialization occurs. This approach allows you to do without using sleep().
     """
     for module in modules:
         __import__(module)
