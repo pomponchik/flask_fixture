1ff84dff43f1333de55f989639fadf0cc106e736
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 04:55:26 +0300

    type hints, docstring

--- flask_fixture/decorator.py
+++ flask_fixture/decorator.py
@@ -5,7 +5,14 @@ from flask_fixture.collection_of_routes import routes
 from flask_fixture.errors import NeedToDefineURLError
 
 
-def endpoint(*args: Any, **kwargs: Any) -> Callable:
+def endpoint(*args: Any, **kwargs: Any) -> Callable[[Callable], Callable]:
+    """
+    A decorator that registers endpoints in the Flask application.
+
+    The URL should always be passed here as the first positional argument. If it is not passed, the NeedToDefineURLError exception will be raised.
+
+
+    """
     if not args:
         raise NeedToDefineURLError('The first argument to the decorator is to pass the expected URL.')
     elif len(args) == 1 and callable(args[0]) and not kwargs:
