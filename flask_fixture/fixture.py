72c78e22d61ebd74792b463b6163e781e4a44a2b
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 04:24:15 +0300

    port variable name

--- flask_fixture/fixture.py
+++ flask_fixture/fixture.py
@@ -9,14 +9,14 @@ from flask_fixture.collection_of_routes import routes
 
 
 @pytest.fixture(scope='session')
-def local_server_url(port: int = 5001) -> str:
+def local_server_url(local_server_port: int = 5001) -> str:
     """
     Run and shutdown the flask server, return a URL of the server.
     """
     modules = {x.module for x in routes}
 
     queue = multiprocessing.Queue()
-    process = multiprocessing.Process(target=run_flask, args=(queue, port, modules))
+    process = multiprocessing.Process(target=run_flask, args=(queue, local_server_port, modules))
     process.start()
     queue.get()
     yield f'http://localhost:{port}/'
