3ddc0365e87c41e355703d9194d3dab25985809a
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 05:02:37 +0300

    empty lines

--- flask_fixture/fixture.py
+++ flask_fixture/fixture.py
@@ -12,10 +12,12 @@ def local_server_url(local_server_port: int = 5001) -> str:
     Run and shutdown the flask server, return a URL of the server.
     """
     modules = {x.module for x in routes}
-
     queue = multiprocessing.Queue()
+
     process = multiprocessing.Process(target=run_flask, args=(queue, local_server_port, modules))
     process.start()
     queue.get()
+
     yield f'http://localhost:{local_server_port}/'
+    
     process.terminate()
