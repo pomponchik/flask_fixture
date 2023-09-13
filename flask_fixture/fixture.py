63e393765acc33a5840f7111f5182898e23e633d
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 05:08:18 +0300

    docstring

--- flask_fixture/fixture.py
+++ flask_fixture/fixture.py
@@ -9,7 +9,14 @@ from flask_fixture.collection_of_routes import routes
 @pytest.fixture(scope='session')
 def local_server_url(local_server_port: int = 5001) -> str:
     """
-    Run and shutdown the flask server, return a URL of the server.
+    A fixture that starts and then stops the Flask server through a separate process.
+
+    A queue is used to synchronize with the generated process.
+    The current process makes sure that initialization in the generated process has ended by receiving a message from the queue.
+
+    The names of modules that need to be imported so that all @endpoint decorators are executed are also passed to the generated process.
+
+    The fixture returns the URL of the server, where you can immediately make requests, taking into account the registered routes.
     """
     modules = {x.module for x in routes}
     queue = multiprocessing.Queue()
@@ -19,5 +26,5 @@ def local_server_url(local_server_port: int = 5001) -> str:
     queue.get()
 
     yield f'http://localhost:{local_server_port}/'
-    
+
     process.terminate()
