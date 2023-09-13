2c59e68ed54c479a8955a40210bfb208ee6110d2
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-13 04:33:11 +0300

    less timeout

--- tests/smokes/test_fixture.py
+++ tests/smokes/test_fixture.py
@@ -2,6 +2,6 @@ import requests
 import pytest
 
 
-@pytest.mark.timeout(180)
+@pytest.mark.timeout(30)
 def test_run_server(local_server_url):
     assert requests.get(local_server_url).text == 'kek'
