bc276ca1cdad54bb41a6a3df3dad6c46f684462d
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-14 15:50:14 +0300

    delete extra code

--- flask_fixture/running_startup_result.py
+++ flask_fixture/running_startup_result.py
@@ -12,6 +12,3 @@ class RunningStartupResult:
     exception_name: Optional[str] = None
     exception_string: Optional[str] = None
     routes: Optional[list[str]] = None
-
-    def to_dict(self):
-        return asdict(self)
