39fe76039b81d0776570c3bfb142c4701db0bae8
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-16 13:48:09 +0300

    reading of the stdout

--- flask_fixture/runner.py
+++ flask_fixture/runner.py
@@ -26,7 +26,7 @@ class QueueHandler(logging.Handler):
 @shoot
 def emit_output(queue, buffer, chunk_type):
     while True:
-        string = buffer.getvalue()
+        string = buffer.read()
         if string:
             chunk: ProcessOutputChunk = ProcessOutputChunk(value=string, type=chunk_type)
             queue.put(chunk)
@@ -76,8 +76,8 @@ def run_flask(queue: Queue, port: int, modules: List[str], configs: Dict[str, An
 
     buffer_stdout = io.StringIO()
     buffer_stderr = io.StringIO()
-    #emit_output(queue, buffer_stdout, ChunkType.STDOUT)
-    #emit_output(queue, buffer_stderr, ChunkType.STDERR)
+    emit_output(queue, buffer_stdout, ChunkType.STDOUT)
+    emit_output(queue, buffer_stderr, ChunkType.STDERR)
 
     with redirect_stdout(buffer_stdout), redirect_stderr(buffer_stderr):
         app.run(port=configs['port'])
