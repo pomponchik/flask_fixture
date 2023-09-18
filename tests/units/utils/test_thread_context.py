22db9f3cc75c42b1d23153b76df16b7f448dbdfe
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-18 15:00:55 +0300

    test

--- tests/units/utils/test_thread_context.py
+++ tests/units/utils/test_thread_context.py
@@ -8,4 +8,4 @@ def test_created_context_is_going_on():
 def test_stopped_context_is_not_going_on():
     context = ThreadContext()
     context.stop()
-    assert context.keep_on() == True
+    assert context.keep_on() == False
