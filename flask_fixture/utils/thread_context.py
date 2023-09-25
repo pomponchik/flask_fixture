49a70244399a1b6ccba6cf610e118411bb389470
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-25 20:10:36 +0300

    withoutself-maded tokens

--- flask_fixture/utils/thread_context.py
+++
@@ -1,9 +0,0 @@
-class ThreadContext:
-    def __init__(self):
-        self.stopped = False
-
-    def stop(self):
-        self.stopped = True
-
-    def keep_on(self):
-        return not self.stopped
