931449d24f8ebc209035385acf4d552bfb5910c0
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-07 20:58:43 +0300

    explicit reimports

--- flask_fixture/__init__.py
+++ flask_fixture/__init__.py
@@ -1,3 +1,3 @@
-from flask_fixture.decorators.decorator_endpoint import endpoint  # noqa: F401
-from flask_fixture.fixture import local_server_url  # noqa: F401
-from flask_fixture.decorators.decorator_config import config  # noqa: F401
+from flask_fixture.decorators.decorator_endpoint import endpoint as endpoint  # noqa: F401
+from flask_fixture.fixture import local_server_url as local_server_url  # noqa: F401
+from flask_fixture.decorators.decorator_config import config as config  # noqa: F401
